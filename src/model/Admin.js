35747e8870e40398192474ed9d9484b8140accca
TommyLeong1 <122337479+TommyLeong1@users.noreply.github.com>
2023-06-18 05:11:54 +0000

    add Admin.js

---
+++ src/model/Admin.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const validator = require('validator');
+
+const AdminSchema = mongoose.Schema({
+  firstName: {
+    type:String
+  },
+  lastName: {
+    type:String
+  },
+  username: {
+    type: String,
+    required: true,
+    unique:[true, "this username is already exist"]
+  },
+  about: {
+    type:String
+  },
+  dateRegistered : {
+    type :String,
+    default : Date.now,
+  },
+  password: {
+    type:String
+  },
+  passwordSalt: {
+    type:String
+  },
+  email: {
+    type:String,
+    required: true,
+    unique:[true, "this email is already exist"],
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("this email is not valid")
+      }
+    }
+  },
+  avatarURL: {
+    type:String
+  },
+  signUpCode: {
+    type:String,
+    required: true,
+    unique:[true, "this sign up code is already exist"]
+  },
+});
+
+module.exports = mongoose.model("Admin", AdminSchema)
